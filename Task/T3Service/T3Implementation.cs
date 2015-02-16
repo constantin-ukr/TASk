3240c0b6c7fe3b52a87929c4d6a3280d8d44796b
constantin-ukr <constantinsd@gmail.com>
2015-02-16 22:53:12 +0200

    Full Implementation of the interface IT3Interface

--- Task/T3Service/T3Implementation.cs
+++ Task/T3Service/T3Implementation.cs
@@ -6,64 +6,134 @@ using System.Threading.Tasks;
 using System.ServiceModel;
 using System.Runtime.Serialization;
 using Common;
+using DataAccess;
+
 
 namespace T3Service
 {
     public class T3Implementation: IT3Interface
     {
+        /// <summary>
+        /// Отримати типізований список усіх семінарів
+        /// </summary>
+        /// <returns>Список усіх семінарів</returns>
         public List<Seminar> GetSeminars()
         {
-            List<Common.Seminar> allSeminars = new List<Seminar>();
-
-            Seminar sem1 = new Common.Seminar();
-            sem1.ID = 1;
-            sem1.Name =  "C#";
-            
-            Seminar sem2 = new Common.Seminar();
-            sem2.ID = 2;
-            sem2.Name =  "ASP.NET";
-
-            allSeminars.Add(sem1);
-            allSeminars.Add(sem2);
-
-            return allSeminars;
+            return DAL.GetSeminars();
         }
 
+        /// <summary>
+        /// Отримати типізований список усіх лекторів
+        /// </summary>
+        /// <returns>Список усіх лекторів</returns>
         public List<Person> GetPersons()
         {
-            List<Person> allPersons = new List<Person>();
-
-            Person p1 = new Person();
-            p1.ID = 1;
-            p1.Name = "Petro";
-            p1.Address = "34 Dodoshenko st.";
-            p1.Zip = "47003";
-            
-            Person p2 = new Person();
-            p2.ID = 1;
-            p2.Name = "Vasya";
-            p2.Address = "11 Dodoshenko st.";
-            p2.Zip = "47005";
-
-            allPersons.Add(p1);
-            allPersons.Add(p2);
-
-            return allPersons;
+            return DAL.GetPersons();
         }
 
+        /// <summary>
+        /// Додати нового запису про лектора в DataAccess
+        /// </summary>
+        /// <param name="newPersons">Типізоване представлення запису лектора</param>
         public void AddPersons(List<Person> newPersons)
         {
-            throw new NotImplementedException();
+            foreach (Person person in newPersons)
+            {
+                DAL.AddNewPerson(person);
+            }
         }
 
+        /// <summary>
+        /// Видалення запису про лектора в DataAccess
+        /// </summary>
+        /// <param name="remPersons">Типізоване представлення запису лектора</param>
         public void RemovePersons(List<Person> remPersons)
         {
-            throw new NotImplementedException();
+            foreach (Person person in remPersons)
+            {
+                DAL.DaletePerson(person);
+            }
         }
 
-        public void UpdatePersons(List<Person> updPersons)
+        /// <summary>
+        /// Оновлення типізованого запису лектора
+        /// </summary>
+        /// <param name="newPersons">Запис який зберігає нові дані</param>
+        /// <param name="oldPersons">Запис який зберігає старі дані</param>
+        public void UpdatePersons(List<Person> newPersons, List<Person> oldPersons)
         {
-            throw new NotImplementedException();
+            foreach (Person newPerson in newPersons)
+            {
+                foreach (Person oldPerson in oldPersons)
+                {
+                    if (newPerson.ID == oldPerson.ID)
+                    {
+                        if (newPerson.Name != oldPerson.Name
+                            || newPerson.Birthday != oldPerson.Birthday
+                            || newPerson.Address != oldPerson.Address
+                            || newPerson.City != oldPerson.City
+                            || newPerson.Zip != oldPerson.Zip
+                            || newPerson.Tax != oldPerson.Tax
+                            || newPerson.Memo != oldPerson.Memo)
+                        {
+                            DAL.UpdatePerson(newPerson);
+                        }
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+            }
+        }
+        
+        /// <summary>
+        /// Створення асоціації між семінаром та списком лекторів
+        /// </summary>
+        /// <param name="sem">Типізоване представлення запису семінар</param>
+        /// <param name="persons">Типізований список лекторів</param>
+        public void AddAssociationSem_Per(Seminar sem, List<Person> persons)
+        {
+            StringBuilder IDs = new StringBuilder();
+            for (int i = 0, len = persons.Count; i < len; i++)
+            {
+                IDs.Append(persons[i].ID + ", ");
+            }
+
+            DAL.AddAssociationSemToPer(sem.Name, IDs.Remove(IDs.Length-1, 1).ToString());
+        }
+
+        /// <summary>
+        ///  Створення асоціації між лектором та списком семінарів
+        /// </summary>
+        /// <param name="person">Типізоване представлення запису лектор</param>
+        /// <param name="seminars">Типізований список семінарів</param>
+        public void AddAssociationPer_Sem(Person person, List<Seminar> seminars)
+        {
+            StringBuilder IDs = new StringBuilder();
+            for (int i = 0, len = seminars.Count; i < len; i++)
+            {
+                
+                IDs.Append(seminars[i].ID + ", ");
+            }
+
+            DAL.AddAssociationPerToSem(person.ID, IDs.Remove(IDs.Length - 1, 1).ToString());
+        }
+
+        /// <summary>
+        /// Видалення асоціації між лектором та списком семінарів
+        /// </summary>
+        /// <param name="person">Типізоване представлення запису лектор</param>
+        /// <param name="seminars">Типізований список семінарів</param>
+        public void RemoveAssociationPer_Sem(Person person, List<Seminar> seminars)
+        {
+            StringBuilder IDs = new StringBuilder();
+            for (int i = 0, len = seminars.Count; i < len; i++)
+            {
+                IDs.Append(seminars[i].ID + ", ");
+            }
+
+            DAL.DeleteAssociationPerToSem(person.ID, IDs.Remove(IDs.Length - 1, 1).ToString());
         }
     }
 }
