6ee99f151910c2c8d6624128fb5f468854e6dba7
constantin-ukr <constantinsd@gmail.com>
2015-02-16 22:44:28 +0200

    Add Provider project

---
+++ Task/Provider/Provider.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.Odbc;
+using System.Data.OleDb;
+using System.Data.SqlClient;
+using System.Data.Common;
+
+namespace Provider
+{
+    public class Provider
+    {
+        #region Private Fields
+        private IDbCommand _idbCommand = null;
+        private IDbConnection _idbConnection = null;
+        private DbParameter _dbParameter = null;
+        #endregion
+
+        #region Public Settings
+        public IDbCommand DbCommand
+        {
+            get
+            {
+                if (_idbCommand != null)
+                    return _idbCommand;
+                return null;
+            }
+            private set { _idbCommand = value; }
+        }
+
+        public IDbConnection DbConnection
+        {
+            get
+            {
+                if (_idbConnection != null)
+                    return _idbConnection;
+                return null;
+            }
+            private set { this._idbConnection = value; }
+        }
+
+        public DbParameter DbParameter
+        {
+            get
+            {
+                if (_dbParameter != null)
+                    return _dbParameter;
+                return null;
+            }
+            private set { this._dbParameter = value; }
+        }
+        #endregion
+
+        public Provider()
+        {           
+            this.OnInit(Setting.GetProviderName());
+        }
+
+        private void OnInit(string providerName)
+        {
+            if (providerName == "System.Data.SqlClient")
+            {
+                DbCommand = new SqlCommand();
+                DbConnection = new SqlConnection(Setting.ConnectionString);
+                DbParameter = new SqlParameter();
+            }
+            else if (providerName == "System.Data.Odbc")
+            {
+                DbCommand = new OdbcCommand();
+                DbConnection = new OdbcConnection(Setting.ConnectionString);
+                DbParameter = new OdbcParameter();
+            }
+            else if (providerName == "System.Data.OleDb")
+            {
+                DbCommand = new OleDbCommand();
+                DbConnection = new OleDbConnection(Setting.ConnectionString);
+                DbParameter = new OleDbParameter();
+            }
+        }
+
+
+    }
+}
---
+++ Task/Provider/Setting.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace Provider
+{
+    public static class Setting
+    {
+        //1. Отримати шлях до файлу
+        //2. Знайти значення параметра ProviderName
+        //3. Отримати стрічку підключення
+
+        internal static string ConnectionString
+        {
+            get
+            {
+                if (_connection == null)
+                    throw new NullReferenceException("не вдалося знайти стрічку підключення");
+                return _connection;
+            }
+        }
+        private static string _exePath = GetExePath();
+        private static readonly string _connection = GetConnectionStringByProvider(GetConnStrName(), GetProviderName());
+
+
+        // повертає повний шлях з назвою файлу
+        public static string GetExePath()
+        {
+#if DEBUG
+            string applicationName =
+                Environment.GetCommandLineArgs()[0];
+#else
+            string applicationName =
+                Environment.GetCommandLineArgs()[0] + ".exe";
+#endif
+
+            string exePath = System.IO.Path.Combine(
+                Environment.CurrentDirectory, applicationName);
+
+
+
+            return exePath.Replace(".exe.exe", ".exe");
+        }
+
+
+        /// <summary>Пошук назви провайдера в файлі конфігурації</summary>
+        /// <returns>Текстове представлення властивості ProviderName в appSettings</returns>
+        internal static string GetProviderName()
+        {
+            return GetSettings("ProviderName");
+        }
+        internal static string GetConnStrName()
+        {
+            return GetSettings("ConnectionStringName");
+        }
+
+        private static string GetSettings(string keyName)
+        {
+            string value = null;
+
+            try
+            {
+                Configuration config =
+                    ConfigurationManager.OpenExeConfiguration(_exePath);
+
+                AppSettingsSection sections =
+                    config.GetSection("appSettings") as AppSettingsSection;
+
+                if (sections != null)
+                    foreach (var key in sections.Settings.AllKeys)
+                    {
+                        if (sections.Settings[key].Key == keyName)
+                        {
+                            value = sections.Settings[key].Value;
+                            break;
+                        }
+                    }
+                else
+                {
+                    throw new ArgumentNullException();
+                }
+            }
+            catch (Exception ex)
+            {
+                //_service.ShowError(ex.Message + "\n" + ex.StackTrace);
+            }
+
+            return value;
+        }
+               
+
+        /// <summary>Повертає стрічку підключення в залежності від типу провайдера</summary>
+        /// <param name="providerName">Текстове представлення провайдера</param>
+        /// <returns>Стрічка підключення</returns>
+        private static string GetConnectionStringByProvider(string conStrName, string provName)
+        {
+            string returnValue = null;
+
+            ConnectionStringSettingsCollection settings =
+                ConfigurationManager.ConnectionStrings;
+
+            if (settings != null)
+            {
+                for (int i = 1, len = settings.Count; i < len; i++)
+                {
+                    if (settings[i].Name == conStrName && settings[i].ProviderName == provName)
+                    {
+                        returnValue = settings[i].ConnectionString;
+                        break;
+                    }
+                }
+            }
+
+            return returnValue;
+        }
+    }
+}
