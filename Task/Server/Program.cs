b4d3eaccc6f60fba2d4df205424790b3e287b13a
Constantin Semenyuk <constantinsd@gmail.com>
2015-02-16 10:59:05 +0200

    Add project 'Server'

---
+++ Task/Server/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Runtime.Serialization;
+using System.ServiceModel;
+
+using Common;
+using T3Service;
+
+namespace Server
+{
+    class Program
+    {
+        static ServiceHost T3Host;
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Start Server\nDate: "+DateTime.Now);
+            Console.ForegroundColor = ConsoleColor.Gray;           
+
+            try
+            {
+                T3Host =  new ServiceHost(typeof(T3Implementation), new Uri[] {new Uri("http://localhost:5000/T3Service")});
+                T3Host.Open();
+            }
+
+            catch
+            {
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
