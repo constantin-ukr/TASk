7b02f246e302f8d2631a297fe9c3252572248313
Constantin Semenyuk <constantinsd@gmail.com>
2015-02-16 10:57:51 +0200

    Add project Common

---
+++ Task/Common/IT3Interface.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Runtime.Serialization;
+using System.ServiceModel;
+
+namespace Common
+{
+    [ServiceContract]
+    public interface IT3Interface
+    {
+        [OperationContract]
+        List<Seminar> GetSeminars();
+
+        [OperationContract]
+        List<Person> GetPersons();
+
+        [OperationContract]
+        void AddPersons(List<Person> newPersons);
+
+        [OperationContract]
+        void RemovePersons(List<Person> remPersons);
+
+        [OperationContract]
+        void UpdatePersons(List<Person> updPersons);
+    }
+}
---
+++ Task/Common/Person.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using System.Runtime.Serialization;
+namespace Common
+{
+    [DataContract]
+    public class Person
+    {
+        [DataMember]
+        public int ID { get; set; }
+        [DataMember]
+        public string Name { get; set; }
+        [DataMember]
+        public DateTime? Birthday { get; set; }
+        [DataMember]
+        public string Address { get; set; }
+        [DataMember]
+        public string Zip { get; set; }
+        [DataMember]
+        public string City { get; set; }
+        [DataMember]
+        public string Tax { get; set; }
+        [DataMember]
+        public string Memo { get; set; }
+        
+        public override string ToString()
+        {
+            return String.Format(
+                "Імя: {0}{7}Дата народження:{1}{7}Адреса:{2}{7}Індекс:{3}{7}Місто:{4}{7}Тах:{5}{7}Опис:{6}{7}",
+                    Name, Birthday, Address, Zip, City, Tax, Memo, Environment.NewLine);
+        }
+    }
+}
